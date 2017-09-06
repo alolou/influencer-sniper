2b4c91618ed225919f13ebef0a510de8b4112829
Liliya Akhtyamova <liliya.akhtyamova@jet.com>
2017-09-06 14:33:45 +0100

    modified files

--- twitterParser.py
+++ twitterParser.py
@@ -1,7 +1,7 @@
 import tweepy
 import sys
 import jsonpickle
-import os
+import os, subprocess
 
 API_KEY    = 'v2sK8SKbCTzrPhUkxYKEB8luX'
 API_SECRET = '2WiEZ5ExP4KFLzUmPxlKifEyPiouISPO5sbqyEddQEHTd8JG8z'
@@ -9,7 +9,7 @@ API_SECRET = '2WiEZ5ExP4KFLzUmPxlKifEyPiouISPO5sbqyEddQEHTd8JG8z'
 auth = tweepy.AppAuthHandler(API_KEY, API_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True,
-				   wait_on_rate_limit_notify=True)
+                   wait_on_rate_limit_notify=True)
 
 if (not api):
     print ("Can't Authenticate")
@@ -21,38 +21,52 @@ tweetsPerQry = 100
 fName = 'tweets.json'
 sinceId = None
 max_id = -1
-
+i = 0
 tweetCount = 0
 print("Downloading max {0} tweets".format(maxTweets))
-with open(fName, 'w') as f:
-    f.write('[' + '\n')
-    while tweetCount < maxTweets:
-        try:
-            if (max_id <= 0):
-                if (not sinceId):
-                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry)
-                else:
-                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
-                                            since_id=sinceId)
+
+all_tweets = []
+
+while tweetCount < maxTweets:
+    try:
+        if (max_id <= 0):
+            if (not sinceId):
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry)
             else:
-                if (not sinceId):
-                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
-                                            max_id=str(max_id - 1))
-                else:
-                    new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
-                                            max_id=str(max_id - 1),
-                                            since_id=sinceId)
-            if not new_tweets:
-                print("No more tweets found")
-                break
-            for tweet in new_tweets:
-                f.write(jsonpickle.encode(tweet._json, unpicklable=False) + ',' + '\n')
-            tweetCount += len(new_tweets)
-            print("Downloaded {0} tweets".format(tweetCount))
-            max_id = new_tweets[-1].id
-        except tweepy.TweepError as e:
-            print("some error : " + str(e))
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        since_id=sinceId)
+            
+        else:
+            if (not sinceId):
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        max_id=str(max_id - 1))
+            else:
+                new_tweets = api.search(q=searchQuery, count=tweetsPerQry,
+                                        max_id=str(max_id - 1),
+                                        since_id=sinceId)
+        if not new_tweets:
+            print("No more tweets found")
             break
+
+        for t in new_tweets:
+            all_tweets.append(jsonpickle.encode(t._json, unpicklable=False))
+
+        tweetCount += len(new_tweets)
+
+        print("Downloaded {0} tweets".format(tweetCount))
+        max_id = new_tweets[-1].id
+    except tweepy.TweepError as e:
+        print("some error : " + str(e))
+        break
+
+print (len(all_tweets))
+print(all_tweets[0])
+
+with open('tweetsN.json', 'w') as f:
+    f.write('[' + '\n')
+    f.write(",\n".join(all_tweets))
     f.write(']')
+    f.close()
+    
 
 print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
